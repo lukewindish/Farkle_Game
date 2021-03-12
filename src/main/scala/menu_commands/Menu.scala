597d30eea3d31be93d9122d2b18361353a5d4927
lukewindish <59708531+lukewindish@users.noreply.github.com>
2021-03-11 20:22:33 -0500

    v0.2.0

--- src/main/scala/menu_commands/Menu.scala
+++ src/main/scala/menu_commands/Menu.scala
@@ -21,14 +21,14 @@ object Menu {
     Board.resetDie
     for (p <- PlayerOrder.toArray){
       p.resetScore
-    } 
+    }
     PlayerOrder.resetOrder
     Board.show
   }
-  
+
   /** Checks if any player has a score over 10000, returns a boolean */
   def CHECK_FOR_WINNER: Boolean = {true}
-  
+
   /** Has player on top of player order execute their turn */
   def DO_MOVE: Unit = {
     val playerUp = PlayerOrder.current
@@ -36,16 +36,16 @@ object Menu {
     var points = 0
     var rolls = 0
     var diceLeft = 6
-    
+
     //first roll
     Board.testRoll
     val firstRoll = Board.calculateScore(diceLeft)
     points += firstRoll._1
     diceLeft = diceLeft - firstRoll._2
-    
+
     var keepRolling = true
     keepRolling = playerStrategy.rollAgain(points,diceLeft)
-    
+
     while (keepRolling && diceLeft > 0) {
       Board.testRoll
       var roll = Board.calculateScore(diceLeft)
@@ -53,22 +53,23 @@ object Menu {
       diceLeft = diceLeft - roll._2
       keepRolling = playerStrategy.rollAgain(points,diceLeft)
     }
+
     playerUp.addScore(points)
     Menu.advancePlayerOrder
   }
   /** Executes a move for each player in the player order unless a winner is declared */
   def DO_TURN: Unit = {}
-  
+
   /** Executes through player moves until a player has a score over 10000 */
   def DO_GAME: Unit = {}
-  
+
   /** Sets a strategy to the designated player
-   *  
+   *
    *  @param playerName Name of player to be given the strategy
    *  @param strategy Strategy to be given to the player
    */
   def setPlayerStrategy(playerName: String,strategy: Strategy): Unit = {}
-  
+
   /** Function returns a string of strategies available for players to use */
   def showStrategies: String = {
     return "fail"
