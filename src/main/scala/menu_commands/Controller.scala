2bc201f9b3425725c8de35acb9f47f627eb71f26
SneakyFox1011 <kyle.krage@westpoint.edu>
2021-03-25 14:39:23 -0400

    gui beginning

--- src/main/scala/menu_commands/Controller.scala
+++ src/main/scala/menu_commands/Controller.scala
@@ -7,7 +7,31 @@ class Controller(view: View, model: Model) {
   def hello = Action("Hello") {
     view.textArea.text = model.getMessage
   }
-
+  
+  /**
+    def showGameAreaClick : Action {
+      
+    }
+    def initializeGameClick : Action {
+      
+    }
+    def doMoveClick : Action {
+      
+    }
+    def doTurnClick : Action {
+      
+    }
+    def doGameClick : Action {
+      
+    }
+    def checkForWinnerClick : Action {
+      
+    }
+    def advancePlayerOrderClick : Action {
+      
+    }
+  **/
+  
   def exit = Action("Exit") {
     sys.exit(0)
   }
--- src/main/scala/menu_commands/View.scala
+++ src/main/scala/menu_commands/View.scala
@@ -1,13 +1,38 @@
 package menu_commands
 
 import scala.swing._
+import java.awt.Color
+import BorderPanel.Position._
 
 class View extends MainFrame {
 
-   var _controller : Option[Controller] = None
-
+    var _controller : Option[Controller] = None
+    object showGameAreaButton extends Button("Show Game Area")
+    object initializeGameButton extends Button("Initialize Game")
+    object doMoveButton extends Button("Do Move")
+    object doTurnButton extends Button("Do Turn")
+    object doGameButton extends Button("Do Game")
+    object checkForWinnerButton extends Button("Check For Winner")
+    object advancePlayerOrderButton extends Button("Advance Player Order")
+    
+    
+    object borderPanel extends BorderPanel {
+      layout += new Label("Current Scores") {
+        background = Color.cyan
+        opaque = true} -> North
+      layout += new Label("Buttons") {
+        background = Color.yellow
+        opaque = true} -> West
+      layout += new Label("Game Text Area") {
+        background = Color.pink
+        opaque = true} -> Center
+      layout += new Label("Player Order") {
+        background = Color.green
+        opaque = true} -> South
+    }
   // Components
   val textArea = new TextArea
+  val scrollPane = new ScrollPane(textArea)
 
   /**
    * View.init
@@ -15,9 +40,18 @@ class View extends MainFrame {
    */
   def init(controller: Controller) {
     _controller = Some(controller)
-    title = "My GUI"
-    contents = textArea
+    title = "Farkle GUI"
+    contents = borderPanel
     centerOnScreen
+    /**
+    showGameAreaButton.action  = controller.showGameAreaClick
+    initializeGameButton.action  = controller.initializeGameClick
+    doMoveButton.action  = controller.doMoveClick
+    doTurnButton.action  = controller.doTurnClick
+    doGameButton.action  = controller.doGameClick
+    checkForWinnerButton.action  = controller.checkForWinnerClick
+    advancePlayerOrderButton.action  = controller.advancePlayerOrderClick
+    **/
 
     menuBar = new MenuBar {
       contents += new Menu("Menu 1") {
@@ -27,6 +61,8 @@ class View extends MainFrame {
         contents += new MenuItem(controller.exit) // end Exit menuItem
       } // end Menu 1
     } // end MenuBar
+    
+
 
     size = new Dimension(500, 500)
     visible = true
