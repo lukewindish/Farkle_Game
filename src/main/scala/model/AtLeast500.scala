3b387ebe2f4aade79df867e229dc9f5b4d39440d
bwh1201 <63434207+bwh1201@users.noreply.github.com>
2021-04-21 22:48:26 -0400

    Update AtLeast500.scala
    update scaladocs

--- src/main/scala/model/AtLeast500.scala
+++ src/main/scala/model/AtLeast500.scala
@@ -8,8 +8,6 @@ class AtLeast500 extends Strategy {
   
   /** Function implements strategy logic 
    *  
-   *  @param pointsSoFar Points player has scored during current move
-   *  @param diceLeft How many dice player has left to roll
    *  @return Boolean for whether or not player should roll again 
    *  
    */
